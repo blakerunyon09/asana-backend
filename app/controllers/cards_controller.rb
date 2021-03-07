61392b3b4d00a1af0de74951238936878190013d
Blake Runyon <blakerunyon09@gmail.com>
2021-03-07 11:49:20 -0700

    More CORs Tests

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,16 +9,16 @@ class CardsController < ApplicationController
   end
   def create
     @card = Card.create(name: params[:name], description: params[:description], user_id: params[:user_id])
-    redirect_to "/"
+    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
   end
   def update
     @card = Card.find(params[:id])
     @card.update(name: params[:name], description: params[:description], user_id: params[:user_id])
-    redirect_to "/"
+    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
   end
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
-    redirect_to "/"
+    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
   end
 end
