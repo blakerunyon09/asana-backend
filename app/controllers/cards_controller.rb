3c2747a901d8b5006f20736de02abb944d978c29
Blake Runyon <blakerunyon09@gmail.com>
2021-03-07 12:00:02 -0700

    Reversed CORs Changes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,24 +1,24 @@
 class CardsController < ApplicationController
   def index
     @cards = Card.all
-    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
+    render json: @cards, status: :accepted
   end
   def show
     @card = Card.find(params[:id])
-    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
+    render json: @card, include: :user, status: :accepted
   end
   def create
     @card = Card.create(name: params[:name], description: params[:description], user_id: params[:user_id])
-    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
+    render json: @card, status: :accepted
   end
   def update
     @card = Card.find(params[:id])
     @card.update(name: params[:name], description: params[:description], user_id: params[:user_id])
-    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
+    render json: @card, status: :accepted
   end
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
-    redirect_to "https://gallant-poitras-e8ca8f.netlify.app"
+    render json: @card, status: :no_content
   end
 end
